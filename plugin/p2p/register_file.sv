2dd92080e8e8d94a20f08d4e4d1f726ecc9fa8a7
Landon Johnson <landonjohnson@wireless-10-145-71-122.public.utexas.edu>
2024-05-20 16:01:24 -0500

    Initial commit

---
+++ plugin/p2p/register_file.sv
@@ -0,0 +1,38 @@
+module register_file #(
+    //I believe that the shell supports 4096 addresses for the 322mhz box 
+    parameter int ADDR_WIDTH = 12,
+    parameter int DATA_WIDTH =  32
+) (
+    //axi_lite_register interface (between system configuration)
+    input wire system_reg_en,
+    input wire system_reg_we,
+    input wire [ADDR_WIDTH - 1 : 0] system_reg_addr,
+    input wire [DATA_WIDTH - 1 : 0] system_reg_din,
+    output logic [DATA_WIDTH - 1 : 0] system_reg_dout,
+
+    //internal read interface
+    input wire internal_read,
+    input wire [ADDR_WIDTH - 1 : 0] internal_reg_addr,
+    output logic [DATA_WIDTH - 1 : 0] internal_reg_out    
+);
+    logic [DATA_WIDTH - 1 : 0] registers [2 ** ADDR_WIDTH];
+
+    always_comb begin
+        system_reg_dout = 0;
+        internal_reg_out = 0;
+        if(system_reg_we && internal_read) begin
+            //read, then write
+            internal_reg_out = registers[internal_reg_addr];
+            registers[internal_reg]
+        end
+        
+        if(system_reg_en) begin
+            //system register read
+            system_reg_dout = registers[system_reg_addr];
+        end
+        if(internal_read) begin
+            //internal read
+            internal_reg_out = registers[internal_reg_addr];
+        end
+    end
+endmodule: register_file
